d33ebc71aef8cda27f8887c966666b71f8ea54f2
Austin Lawless <austindlawless@gmail.com>
2014-10-18 13:12:51 -0500

    [MASTER] added a class and some more content to the main method

--- src/main/java/com/example/one/ExampleOne.java
+++ src/main/java/com/example/one/ExampleOne.java
@@ -5,5 +5,9 @@ public class ExampleOne {
 
     public static void main(String[] args) {
         System.out.println("Hello World.");
+        ExampleRunner defaultRunner = new ExampleRunner();
+        defaultRunner.run();
+        ExampleRunner parametrizedRunner = new ExampleRunner("Aaron & Thomas are learning stuff!");
+        parametrizedRunner.run();
     }
 }
--- src/main/java/com/example/one/ExampleRunner.java
+++ src/main/java/com/example/one/ExampleRunner.java
@@ -1,7 +1,31 @@
 package main.java.com.example.one;
 
-/**
- * Created by austin on 10/18/14.
- */
 public class ExampleRunner {
+
+    private static final String STATIC_STRING = "I never change because I am final. I am shared between all instances of ExampleRunner.";
+
+    private String instanceString;
+
+    /**
+     * default constructor
+     */
+    public ExampleRunner() {
+        instanceString = "default constructor";
+    }
+
+    /**
+     * constructor that takes a string
+     */
+    public ExampleRunner(String parameter) {
+        instanceString = parameter;
+    }
+
+    public void run() {
+        print(STATIC_STRING);
+        print(instanceString);
+    }
+
+    private void print(String s) {
+        System.out.println(s);
+    }
 }
